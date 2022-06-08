3092450c2bfc9abe760c086bc7dec991cd7e789c
esllo <master@esllo.com>
2022-06-08 15:36:50 +0900

    feat: apply aos to post list

--- src/components/contents/index.jsx
+++ src/components/contents/index.jsx
@@ -1,10 +1,17 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 
 import { ThumbnailContainer } from '../thumbnail-container'
 import { ThumbnailItem } from '../thumbnail-item'
 import { CATEGORY_TYPE } from '../../constants'
+import aos from 'aos'
+
+import 'aos/dist/aos.css'
 
 export const Contents = ({ posts, countOfInitialPost, count, category }) => {
+  useEffect(() => {
+    aos.init({ mirror: true, once: false, offset: 0 })
+  }, [])
+
   const refinedPosts = useMemo(() =>
     posts
       .filter(
@@ -18,7 +25,7 @@ export const Contents = ({ posts, countOfInitialPost, count, category }) => {
   return (
     <ThumbnailContainer>
       {refinedPosts.map(({ node }, index) => (
-        <ThumbnailItem node={node} key={`item_${index}`} />
+        <ThumbnailItem node={node} key={`item_${node.fields.slug}`} />
       ))}
     </ThumbnailContainer>
   )
