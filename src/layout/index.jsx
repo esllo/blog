5f996f8c9f7d5efeca4f10e8e379e9830b823690
esllo <master@esllo.com>
2022-06-08 15:28:26 +0900

    style: change top component position to fixed

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -18,6 +18,7 @@ export const Layout = ({ location, title, children }) => {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
+          marginTop: 60,
           maxWidth: rhythm(30),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
